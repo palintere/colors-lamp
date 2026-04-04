9010007ec972973b1f6bdf45b1443b4b1850f80f
palintere <182365392+palintere@users.noreply.github.com>
2026-04-04 14:42:43 -0400

    stop flatcompat

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -10,7 +10,11 @@ module.exports = [
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error",
-      "semi": ["error", "always"],
+      "no-console": "warn",
+      "no-debugger": "error",
+      "no-cond-assign": "error",
+      "no-constant-condition": "error",
+      // …add all eslint:recommended rules here
     },
   },
 ];
